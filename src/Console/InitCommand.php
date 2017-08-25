c63a886f78e546ffd1db410c527a5808ea2e721e
Taha Kelesoglu <taha.kelesoglu@gmail.com>
2017-08-25 09:16:52 +0300

    Init command fix.

--- src/Console/InitCommand.php
+++ src/Console/InitCommand.php
@@ -31,7 +31,8 @@ class InitCommand extends Command
             $this->scope->output->write('Creating crust-file.php');
             touch('./crust-file.php');
             $renderer = new \Mustache_Engine();
-            $content = $renderer->render(__DIR__ . '/../Factory/Templates/crust-file.php.mustache', array());
+            $tpl = file_get_contents(__DIR__ . '/../Factory/Templates/crust-file.php.mustache');
+            $content = $renderer->render($tpl, array());
             file_put_contents('./crust-file.php', $content);
             $this->scope->output->writeln(' <success>✓</success>');
         }
